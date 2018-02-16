50ee386a4c82b855f24b9b7e1e12ea03cb550c4c
Malika Sinkler <Quietstorm973@Malikas-MacBook-Air.local>
2018-02-16 01:09:24 -0500

    Code added to js file

---
+++ todo.js
@@ -0,0 +1,14 @@
+var toDoList = {
+
+  toDos: [{item:"clean", done: false }, {item:"run to the bank", done: true}];
+
+  addTodo: function(toDoItem) {
+   for (i = 0; i < toDoList.lenght; i++)
+  }
+
+  //unsure what to do next
+ completeTodo: function(complete) {
+   if toDoList === true;
+
+ }
+}
